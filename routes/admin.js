d1bfa7c4cf90a9266f98cc9a4828d167ba4b83fa
bledsoef <bledsoef@berea.edu>
2022-10-25 20:56:13 -0400

    started pug integration

--- routes/admin.js
+++ routes/admin.js
@@ -3,15 +3,18 @@ const express = require('express')
 const router = express.Router()
 const rootDir = require('../util/path')
 
+const products = []
+
 // /admin/add-products => GET
 router.get('/add-product', (req, res, next) => {
     res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
-});
+})
 
 // /admin/add-products => POST
 router.post('/add-product', (req, res, next) => {
-    console.log(req.body)
+    products.push({title:req.body.title})
     res.redirect("/")
 })
 
-module.exports = router
+exports.router = router
+exports.products = products
--- routes/shop.js
+++ routes/shop.js
@@ -2,10 +2,13 @@ const path = require('path')
 const express = require('express')
 const router = express.Router()
 const rootDir = require('./../util/path')
+const adminData = require("./admin")
 
 // / => GET
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+    // console.log(adminData.products)
+    // res.sendFile(path.join(rootDir, 'views', 'shop.'))
+    res.render('shop')
 })
 
 module.exports = router
